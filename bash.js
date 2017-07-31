849aeff3a4cd7ee6b6459d1e0bc7af8daf3f52bf
An Le <lean@Ans-MacBook-Air.local>
2017-07-31 14:37:49 -0400

    Working version

--- bash.js
+++ bash.js
@@ -1,18 +1,19 @@
 var commands = require('./commands');
-// Output a prompt
-process.stdout.write('prompt > ');
+var chalk = require('chalk');
 
+// Output a prompt
+process.stdout.write(chalk.yellow('prompt > '));
+var done = function(output) {
+  console.log(chalk.green(output));
+  process.stdout.write('prompt > ');
+}
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
-  commands[cmd]();
-  // if (cmd === 'pwd') {
-  //   process.stdout.write(process.env.PWD);
-  // }
-  //
-  // if (cmd === 'date'){
-  //   var dateNow = new Date().toString();
-  //   process.stdout.write(dateNow)
-  // }
-
+  data = data.toString().trim();
+  var arrArgs = data.split(' ');
+  var cmd = arrArgs[0];
+  var args = arrArgs.slice(1).join(' ');
+  // if (!commands[cmd](args)) { process.stdout.write(chalk.red('Please check your input'));
+  // process.stdout.write(chalk.yellow('\nprompt > ')); }
+  commands[cmd](args, done);
 });
--- commands.js
+++ commands.js
@@ -1,11 +1,56 @@
+var fs = require('fs');
+var chalk = require('chalk');
 module.exports = {
-  pwd: function(){
-      process.stdout.write(process.env.PWD);
-      process.stdout.write('prompt > ');
+  pwd: function(data, done){
+    var output = process.env.PWD;
+    done(output);
   },
-  date: function(){
+  date: function(data, done){
     var dateNow = new Date().toString();
-    process.stdout.write(dateNow)
-    process.stdout.write('prompt > ');
+    done(dateNow);
+  },
+  ls: function(file, done) {
+    var output = "";
+    fs.readdir('.', function(err, files) {
+      files.forEach(function(file) {
+        output += file.toString() + "\n";
+      })
+      done(output);
+    });
+  },
+  echo: function(data, done){
+    if (data[0] !== '$') {
+      done(data);
+    } else {
+      for (var prop in process.env) {
+        // console.log(data, prop);
+        if (data.slice(1) === prop) {
+          done(process.env[prop]);
+        }
+      }
+    }
+  },
+  cat: function(fileName, done){
+    // fileName = `./${fileName}`;
+    fs.readFile(fileName, 'utf-8', function(err, content){
+      if (err) throw err;
+      done(content);
+    })
+  },
+  head: function(fileName, done){
+    // fileName = `./${fileName}`;
+    fs.readFile(fileName, 'utf-8', function(err, content){
+      if (err) throw err;
+      content = content.split('\n');
+      done(content.slice(0,5).join('\n'));
+    })
+  },
+  tail: function(fileName, done){
+    fileName = `./${fileName}`;
+    fs.readFile(fileName, 'utf-8', function(err, content){
+      if (err) throw err;
+      content = content.split('\n');
+      done(content.slice(-6).join('\n'));
+    })
   }
 }
