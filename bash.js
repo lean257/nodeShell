0bcf8a20ac07b0138020780105729e0a6f0bf426
An Le <lean@Ans-MacBook-Air.local>
2017-07-31 17:23:04 -0400

    piping

--- bash.js
+++ bash.js
@@ -1,19 +1,40 @@
 var commands = require('./commands');
 var chalk = require('chalk');
-
+const prompt = chalk.blue('\nprompt > ');
+var cmdGroups;
 // Output a prompt
-process.stdout.write(chalk.yellow('prompt > '));
-var done = function(output) {
-  console.log(chalk.green(output));
-  process.stdout.write('prompt > ');
-}
+process.stdout.write(prompt);
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  data = data.toString().trim();
-  var arrArgs = data.split(' ');
-  var cmd = arrArgs[0];
-  var args = arrArgs.slice(1).join(' ');
-  // if (!commands[cmd](args)) { process.stdout.write(chalk.red('Please check your input'));
-  // process.stdout.write(chalk.yellow('\nprompt > ')); }
-  commands[cmd](args, done);
+  cmdGroups = data.toString().trim().split(/\s*\|\s*/g); // separate by pipe
+  const unsafeCommands = getUnsafeCommands(cmdGroups);
+  if (unsafeCommands.length) {
+    process.stderr.write(chalk.red('command(s) not found: ' + unsafeCommands.join(' ')));
+    cmdGroups = [];
+    done('');
+  } else {
+    execute(cmdGroups.shift());
+  }
 });
+
+function getUnsafeCommands (cmdStrings) {
+  return cmdStrings
+  .map(cmdString => cmdString.split(' ')[0]) // remove arguments (ES6 arrow)
+  .filter(cmd => !commands[cmd]); // filter down to unsafe commands
+}
+
+function execute (cmdGroup, lastStdout) {
+  const tokens = cmdGroup.toString().trim().split(' '); // separate this cmd and its args
+  const cmd = tokens[0];
+  const args = tokens.slice(1).join(' ');
+  commands[cmd](lastStdout, args, done); // where the magic happens
+}
+
+// handle result of a command (async cannot `return`, only call more funcs)
+function done (stdout) {
+  if (cmdGroups.length) {
+    execute(cmdGroups.shift(), stdout); // execute the next command-arg set
+  } else {
+    process.stdout.write(stdout + prompt);
+  }
+}
--- commands.js
+++ commands.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 var chalk = require('chalk');
 module.exports = {
-  pwd: function(data, done){
+  pwd: function(stdin, data, done){
     var output = process.env.PWD;
     done(output);
   },
-  date: function(data, done){
+  date: function(stdin, data, done){
     var dateNow = new Date().toString();
     done(dateNow);
   },
-  ls: function(file, done) {
+  ls: function(stdin, file, done) {
     var output = "";
     fs.readdir('.', function(err, files) {
       files.forEach(function(file) {
@@ -18,26 +18,30 @@ module.exports = {
       done(output);
     });
   },
-  echo: function(data, done){
+  echo: function(stdin, data, done){
     if (data[0] !== '$') {
       done(data);
     } else {
       for (var prop in process.env) {
-        // console.log(data, prop);
         if (data.slice(1) === prop) {
           done(process.env[prop]);
         }
       }
     }
   },
-  cat: function(fileName, done){
-    // fileName = `./${fileName}`;
-    fs.readFile(fileName, 'utf-8', function(err, content){
-      if (err) throw err;
-      done(content);
+  cat: function(stdin, fileName, done){
+    files = fileName.split(' ');
+    var counter = 0;
+    files.forEach(function(file){
+      fs.readFile(file, 'utf-8', function(err, content){
+        counter++;
+        if (err) throw err;
+        else done(content.toString());
+        if (counter === files.length) { return;}
+      })
     })
   },
-  head: function(fileName, done){
+  head: function(stdin, fileName, done){
     // fileName = `./${fileName}`;
     fs.readFile(fileName, 'utf-8', function(err, content){
       if (err) throw err;
@@ -45,7 +49,7 @@ module.exports = {
       done(content.slice(0,5).join('\n'));
     })
   },
-  tail: function(fileName, done){
+  tail: function(stdin, fileName, done){
     fileName = `./${fileName}`;
     fs.readFile(fileName, 'utf-8', function(err, content){
       if (err) throw err;
